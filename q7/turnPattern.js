5c9e9828ef861f32c57fbc636fc36c6e8dc765e0
angela <awander5324@gmail.com>
2022-04-10 11:34:48 +0800

    q7: 拆分turnPattern function

--- q7/turnPattern.js
+++ q7/turnPattern.js
@@ -10,10 +10,10 @@ export default function turnPattern(customizedPattern, customizedDirection) {
 
   //* 找出原圖形的最寬（＝>找陣列中的，最長的子陣列長度）
   //* 根據寬度：建立空白陣列作為模板
-  const patternLength = getPatternWidth(patternArray, direction);
-  const emptyTemplate = Array(patternLength).fill("");
+  const rotatedPatternWidth = getRotatedPatternWidth(patternArray, direction);
+  const emptyTemplate = Array(rotatedPatternWidth).fill("");
 
-  function getPatternWidth(arrays, direction) {
+  function getRotatedPatternWidth(arrays, direction) {
     if (direction === "up" || direction === "down") {
       return arrays.length;
     } else {
@@ -27,33 +27,36 @@ export default function turnPattern(customizedPattern, customizedDirection) {
 
   //* 根據寬度：將原始圖案的每行陣列變得一樣長
   const padPatternArray = patternArray.map((ele) =>
-    ele.replaceAll(" ", "0").padEnd(patternLength, "0")
+    ele.replaceAll(" ", "0").padEnd(rotatedPatternWidth, "0")
   );
 
-  //* 生成原版愛心的 dot map
-  let originalDotMap = [];
+  //* 根據原始圖形生成座標圖檔
+  const originalDotMap = getPatternDotMap(padPatternArray);
 
-  for (let row = 0; row < padPatternArray.length; row++) {
-    let rowArr = [];
-    for (let column = 0; column < padPatternArray[row].length; column++) {
-      if (padPatternArray[row][column] === "*") {
-        var isStar = 1;
-      } else {
-        var isStar = 0;
+  function getPatternDotMap(padPatternArray) {
+    let originalDotMap = [];
+
+    for (let row = 0; row < padPatternArray.length; row++) {
+      let rowArr = [];
+      for (let column = 0; column < padPatternArray[row].length; column++) {
+        if (padPatternArray[row][column] === "*") {
+          var isStar = 1;
+        } else {
+          var isStar = 0;
+        }
+        rowArr.push(isStar);
       }
-      rowArr.push(isStar);
+      originalDotMap.push(rowArr);
     }
-    originalDotMap.push(rowArr);
+    return originalDotMap;
   }
 
-  //* 生成 reverse 愛心的 dot map (右轉 ＆ 向下轉用)
-  const reverseOriginalDotMap = Object.assign([], originalDotMap).reverse();
-
-  function turn(direction) {
+  function turn(direction, dotMap) {
+    let map = dotMap;
     if (direction === "up" || direction === "left") {
-      var map = originalDotMap;
+      map = dotMap;
     } else {
-      var map = reverseOriginalDotMap;
+      map = dotMap.reverse();
     }
     map.forEach((row, rowIndex) => {
       row.forEach((dot, dotIndex) => {
@@ -73,90 +76,9 @@ export default function turnPattern(customizedPattern, customizedDirection) {
     });
     return emptyTemplate.join("\n");
   }
-  return turn(direction);
+
+  return turn(direction, originalDotMap);
 }
-
-///
-// const pattern = arrow;
-
-// //* 將圖形切割成陣列
-// const patternArray = pattern.split("\n");
-
-// //* 移除頭尾的換行空白
-// patternArray.pop();
-// patternArray.shift();
-
-// const patternLength = getPatternLength(patternArray);
-// const emptyTemplate = Array(patternLength).fill(" ");
-
-// //* 找出原圖形的最寬（＝>找陣列中的，最長子陣列長度）
-// function getPatternLength(arrays) {
-//   const lengthList = arrays.map((array) => array.length);
-//   lengthList.sort(function compare(a, b) {
-//     if (a > b) {
-//       return -1;
-//     }
-//     if (a < b) {
-//       return 1;
-//     }
-//     // a === b
-//     return 0;
-//   });
-
-//   return lengthList[0];
-// }
-
-// const padPatternArray = patternArray.map((ele) =>
-//   ele.replaceAll(" ", "0").padEnd(patternLength, "0")
-// );
-
-// //! 生成原版愛心的 dot map
-// let originalDotMap = [];
-
-// for (let row = 0; row < padPatternArray.length; row++) {
-//   let rowArr = [];
-//   for (let column = 0; column < padPatternArray[row].length; column++) {
-//     if (padPatternArray[row][column] === "*") {
-//       var isStar = 1;
-//     } else {
-//       var isStar = 0;
-//     }
-//     rowArr.push(isStar);
-//   }
-
-//   originalDotMap.push(rowArr);
-// }
-
-// const reverseOriginalDotMap = Object.assign([], originalDotMap).reverse();
-
-// let patternRotated = Object.assign([], emptyTemplate);
-
-// function turn(direction) {
-//   if (direction === "up" || direction === "right") {
-//     var map = originalDotMap;
-//   } else {
-//     var map = reverseOriginalDotMap;
-//   }
-//   map.forEach((row, rowIndex) => {
-//     row.forEach((dot, dotIndex) => {
-//       //為了要讓他可以讀到 rowIndex 和 dotIndex
-//       if (direction === "up" || direction === "down") {
-//         var putOrder = rowIndex;
-//       } else {
-//         var putOrder = dotIndex;
-//       }
-
-//       if (dot === 0) {
-//         patternRotated[putOrder] += " ";
-//       } else {
-//         patternRotated[putOrder] += "*";
-//       }
-//     });
-//   });
-//   return patternRotated.join("\n");
-// }
-
-// console.log(turn("down"));
 
 //* 翻轉對照
 // const directionArg = {
